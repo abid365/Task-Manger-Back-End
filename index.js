023a15da10b7b839389a715e18910893b6028257
jawad Bin Azam <abidibnazam@gmail.com>
2023-11-30 16:43:44 +0600

    delete by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
@@ -58,6 +58,15 @@ async function run() {
     //get pending todos
     app.get("/pending", async (req, res) => {
       const result = await todoCollectionPending.find().toArray();
+      res.send(result);
+    });
+
+    // delete item from pending route
+    app.delete("/pending/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(`please delete from database , id:${id}`);
+      const query = { _id: new ObjectId(id) };
+      const result = await todoCollectionPending.deleteOne(query);
       res.send(result);
     });
 
